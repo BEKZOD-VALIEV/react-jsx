1b26f216259ca55a681a7ea3c5209ea87ed3bb43
BEKZOD-VALIEV <bekzodvaliev4@gmail.com>
2023-04-07 06:45:26 +0900

    changed to JSX

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>by Bek</p>
+    </div>
+  );
+}
+
+export default App;
