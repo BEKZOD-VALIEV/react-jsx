0a87113211603cd46a8d51efffd7b78244642215
BEKZOD-VALIEV <bekzodvaliev4@gmail.com>
2023-04-07 07:36:02 +0900

    I changed Sof React to JSX

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 
-const Book = ({name, year, price}) => {
+const News = (props) => {
   return (
     <div>
-      <h2>{name}</h2>
-      <h2>{year}</h2>
-      <h2>{price}</h2>
+      <h1>{props.name}</h1>
+      <div className="box">
+        <pre>{props.extra}</pre>
+        <p>{props.text}</p>
+      </div>
+      <div className="square">
+        <h1>{props.header}</h1>
+      </div>
     </div>
   )
 }
@@ -16,14 +21,14 @@ const Book = ({name, year, price}) => {
 const App = () => {
   return (
     <div>
-      <Book name = "JS for beginners" year = "2020" price = "50"/>
-      <Book name = "React for beginners" year = "2021" price = "60"/>
-      <Book name = "Vue for beginners" year = "2022" price = "70"/>
+      <News 
+        name = "BBC News" 
+        extra = "2020" 
+        text = "Bekzod Valiev" 
+        header = "BigJob"/>
     </div>
   )
 }
-
-// ReactDOM.render(React.createElement(App), document.getElementById('root'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
